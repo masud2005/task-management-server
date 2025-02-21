7d45398bcb1b0812cacb7a6b9e6151673e956e90
masud2005 <masud20012005@gmail.com>
2025-02-21 11:00:11 +0600

    Store Users to the Database and Get All Users APIs Added

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const usersCollection = client.db('taskManagement').collection('users');
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
+        // Store user to database
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
