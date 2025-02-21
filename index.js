7f42ee408e1f02ffd0d6d2342e1ae4da4b077cd7
masud2005 <masud20012005@gmail.com>
2025-02-21 11:38:18 +0600

    Store task to the Database and Get All Task APIs Added

--- index.js
+++ index.js
@@ -27,16 +27,31 @@ async function run() {
         await client.connect();
 
         const usersCollection = client.db('taskManagement').collection('users');
+        const tasksCollection = client.db('taskManagement').collection('all-task');
 
+        // Get All Users
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
 
-        // Store user to database
+        // Store user to the database
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        // Store task to the database
+        app.post('/all-task', async (req, res) => {
+            const task = req.body;
+            const result = await tasksCollection.insertOne(task);
+            res.send(result);
+        })
+
+        // // Get All Task
+        app.get('/all-task', async (req, res) => {
+            const result = await tasksCollection.find().toArray();
             res.send(result);
         })
 
